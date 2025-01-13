b4efb8671840a423db76da817561013643da5c38
Agus Puryanto <aguspuryanto@gmail.com>
2025-01-13 16:40:21 +0700

    update bpjs

--- app/Controllers/BpjsController.php
+++ app/Controllers/BpjsController.php
@@ -18,4 +18,45 @@ class BpjsController extends BaseController
 
         return $this->response->setJSON($response);
     }
+
+    // https://faskes.bpjs-kesehatan.go.id/aplicares/Referensi/propinsi
+    public function getPropinsi()
+    {
+        $bpjs = new BpjsHelper();
+
+        $response = $bpjs->sendRequest('Referensi/propinsi', 'GET');
+
+        return $this->response->setJSON($response);
+    }
+
+    // https://faskes.bpjs-kesehatan.go.id/aplicares/Referensi/dati2/{id}
+    public function getDati2($id)
+    {
+        $bpjs = new BpjsHelper();
+
+        $response = $bpjs->sendRequest('Referensi/dati2/' . $id, 'GET');
+
+        return $this->response->setJSON($response);
+    }
+
+    // https://faskes.bpjs-kesehatan.go.id/aplicares/Pencarian/getList
+    // {\"params\":{},\"jnscari\":\"carifaskes\",\"jnscari1\":\"bylocation\",\"dati2ppk\":\"0226\",\"jnsppk\":\"P\",\"lat\":-8.6562761,\"lng\":115.15932969999999}
+    public function getList()
+    {
+        $bpjs = new BpjsHelper();
+
+        $data = [
+            "params" => [],
+            "jnscari" => "carifaskes",
+            "jnscari1" => "bylocation",
+            "dati2ppk" => "0226",
+            "jnsppk" => "P",
+            "lat" => -8.6562761,
+            "lng" => 115.15932969999999
+        ];
+
+        $response = $bpjs->sendRequest('Pencarian/getList', 'POST', $data);
+
+        return $this->response->setJSON($response);
+    }
 }
